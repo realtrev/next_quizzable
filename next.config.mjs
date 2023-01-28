e7000ab73a35478a81f8da142f96434695a5a0a3
Paridax <tgavdaly@gmail.com>
2023-01-27 23:33:02 -0500

    add security headers to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -5,17 +5,61 @@
  */
 !process.env.SKIP_ENV_VALIDATION && (await import("./env/server.mjs"));
 
+const securityHeaders = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=31536000; includeSubDomains; preload",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "same-origin",
+  },
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=(), interest-cohort=()",
+  },
+];
+
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-  /* If trying out the experimental appDir, comment the i18n config out
-   * @see https://github.com/vercel/next.js/issues/41980 */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/login",
+      },
+    ];
+  },
+  // Access-Control-Allow-Origin: *
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
   },
   experimental: {
     appDir: true,
   },
 };
+
 export default config;
