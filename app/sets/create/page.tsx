d02c824306f98ad2fb8fb3b0b25bd367b426057a
Paridax <tgavdaly@gmail.com>
2023-02-12 22:14:43 -0500

    move type declarations, add ability to save set

--- app/sets/create/page.tsx
+++ app/sets/create/page.tsx
@@ -1,68 +1,50 @@
 "use client";
 import PocketBase from "pocketbase";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import Loading from "../../loading";
-import { BiImport, BiShow } from "react-icons/bi";
-import { BsKeyboard } from "react-icons/bs";
-import { MdOutlineSwapHoriz } from "react-icons/md";
 
-type Set = {
-  id: string;
-  title: string;
-  description: string;
-  created: string;
-  updated: string;
-  author: string;
-  content: {
-    termlang: string;
-    deflang: string;
-    pairs: Array<[string, string]>; // [question, answer]
-  };
-  cards: Array<string>;
-  expand: {
-    author: User;
-    cards: Array<Card>;
-  };
-};
-
-type Card = {
-  id: string;
-  term: string;
-  definition: string;
-  created: string;
-  updated: string;
-  set: string;
-  expand: object;
-  image: string;
-};
-
-type User = {
-  avatar: string;
-  collectionId: string;
-  collectionName: string;
-  created: string;
-  email: string;
-  emailVisibility: boolean;
-  expand: {
-    favoriteSets: Array<Set>;
-    sets: Array<Set>;
-  };
-  id: string;
-  name: string;
-  updated: string;
-  username: string;
-  verified: boolean;
-};
+import type { User, EditedSet, EditedCard } from "../../types";
 
 function Page({ params }: { params: { setId: string } }) {
   const router = useRouter();
   const pb = new PocketBase("https://quizzable.trevord.live");
   const [loading, setLoading] = useState(true);
   const [userData, setUserData] = useState(null as User | null);
-  const [setData, setSetData] = useState(null as Set | null);
+
+  let cardData = new Array(3).fill({}).map(() => ({
+    term: "",
+    definition: "",
+    id: "",
+    setId: "",
+    created: "",
+    updated: "",
+    set: "",
+    expand: {},
+    image: "",
+    isEdited: true,
+    isDeleted: false,
+  })) as EditedCard[];
+
+  let set = {
+    id: "",
+    title: "",
+    description: "",
+    visibility: "public",
+    created: "",
+    updated: "",
+    author: "",
+    expand: {
+      author: {} as User,
+      cards: cardData,
+    },
+    cards: [],
+    isEdited: true,
+    isDeleted: false,
+  } as EditedSet;
 
   useEffect(() => {
+
     console.log(params.setId);
 
     const func = async function () {
@@ -88,6 +70,40 @@ function Page({ params }: { params: { setId: string } }) {
     func().catch(console.error);
   }, []);
 
+  function editSet(
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    field: "title" | "description" | "visibility"
+  ) {
+    set[field] = event.target?.value;
+    set.isEdited = true;
+  }
+
+  function editCard(
+    event: ChangeEvent<HTMLInputElement>,
+    card: EditedCard,
+    field: "term" | "definition" | "image"
+  ) {
+    // find the card in the array
+    const index = cardData.findIndex((c) => c.id === card.id);
+    if (index === -1) {
+      console.error("Card not found");
+      return;
+    }
+    cardData[index][field] = event.target.value;
+    cardData[index].isEdited = true;
+    return cardData[index] as EditedCard;
+  }
+
+  function deleteCard(card: EditedCard) {
+    // find the card in the array
+    const index = cardData.findIndex((c) => c.id === card.id);
+    if (index === -1) {
+      console.error("Card not found");
+      return;
+    }
+    cardData[index].isDeleted = true;
+  }
+
   if (loading) {
     return (
       <div className="flex min-h-screen w-full items-center justify-center">
@@ -99,6 +115,9 @@ function Page({ params }: { params: { setId: string } }) {
   if (!userData) {
     return <div className="min-h-screen w-full">User data not found</div>;
   }
+
+      window.set = set;
+    window.pb = pb;
 
   return (
     <div className="min-h-screen w-full">
@@ -119,9 +138,25 @@ function Page({ params }: { params: { setId: string } }) {
             <label className="col-span-1 flex h-10 items-center justify-center rounded-md bg-gray-100">
               <h1 className="font-normal">Definition</h1>
             </label>
-            <CardElement />
-            <CardElement />
-            <CardElement />
+            {cardData.map((card: EditedCard, i: number) => (
+              <CardElement
+                editCard={editCard}
+                deleteCard={deleteCard}
+                saveSet={(autoSave?: boolean) => {
+                  console.log("Save");
+                }}
+                key={i}
+                card={card}
+              />
+            ))}
+            <button
+              className="col-span-2 flex h-16 items-center justify-center rounded-md bg-gray-100 transition-all duration-200 hover:bg-gray-200 hover:shadow-md"
+              onClick={() => {
+                console.log("Create card");
+              }}
+            >
+              <h1 className="font-normal">Create Card</h1>
+            </button>
           </div>
         </main>
         <aside className="w-3/12 shrink-0">
@@ -130,41 +165,14 @@ function Page({ params }: { params: { setId: string } }) {
               className="block h-10 w-full appearance-none rounded-md border border-gray-300 bg-gray-100 px-4 leading-normal outline-none hover:border-gray-400 focus:border-blue-500 focus:bg-gray-200"
               type="text"
               placeholder="Give your set a title!"
+              onChange={(e) => editSet(e, "title")}
             />
 
             <textarea
               className="block h-[7.5rem] w-full resize-none appearance-none rounded-md border border-gray-300 bg-gray-100 py-2 px-4 leading-normal outline-none hover:border-gray-400 focus:border-blue-500 focus:bg-gray-200"
               placeholder="Enter a description..."
+              onChange={(e) => editSet(e, "description")}
             />
-
-            <div className="grid grid-cols-4 gap-3">
-              <button className="col-span-1 flex aspect-square items-center justify-center rounded-md bg-blue-500 font-bold text-white transition-all duration-200 hover:bg-blue-700">
-                <h1 className="text-2xl">
-                  <MdOutlineSwapHoriz />
-                </h1>
-                {/* swap term and definitions */}
-              </button>
-              <button className="col-span-1 flex aspect-square items-center justify-center rounded-md bg-blue-500 font-bold text-white transition-all duration-200 hover:bg-blue-700">
-                <h1 className="text-2xl">
-                  <BiImport />
-                </h1>
-                {/* import */}
-              </button>
-              <button className="col-span-1 flex aspect-square items-center justify-center rounded-md bg-blue-500 font-bold text-white transition-all duration-200 hover:bg-blue-700">
-                <h1>
-                  <h1 className="text-2xl">
-                    <BiShow />
-                  </h1>
-                </h1>
-                {/* visibility */}
-              </button>
-              <button className="col-span-1 flex aspect-square items-center justify-center rounded-md bg-blue-500 font-bold text-white transition-all duration-200 hover:bg-blue-700">
-                <h1 className="text-2xl">
-                  <BsKeyboard />
-                </h1>
-                {/* shortcut menu */}
-              </button>
-            </div>
 
             <div className="grid h-10 grid-cols-2 gap-3">
               <button className="col-span-1 h-full rounded-md bg-gray-400 px-4 font-bold text-white transition-all duration-200 hover:bg-gray-600">
@@ -181,10 +189,51 @@ function Page({ params }: { params: { setId: string } }) {
   );
 }
 
-function CardElement() {
+function CardElement(props: {
+  editCard(
+    event: ChangeEvent<HTMLInputElement>,
+    card: EditedCard,
+    field: "term" | "definition" | "image"
+  ): EditedCard;
+  deleteCard(card: EditedCard): void;
+  saveSet(autoSave?: boolean): void;
+  card: EditedCard;
+}) {
+  let card = props.card;
   return (
-    <div className="col-span-2 h-40 rounded-md border border-gray-300 bg-gray-100 p-5 transition-shadow hover:shadow-md">
-      Card
+    <div className="p10 col-span-2 grid grid-cols-2 gap-5 rounded-md border border-gray-300 bg-gray-100 p-5 transition-shadow hover:shadow-md">
+      {/* term input */}
+      <input
+        className="col-span-1 h-10 appearance-none rounded-md border border-gray-300 bg-gray-100 px-4 leading-normal outline-none hover:border-gray-400 focus:border-blue-500 focus:bg-gray-200"
+        type="text"
+        placeholder="Enter a term..."
+        onChange={(e) => {
+          card = props.editCard(e, card, "term");
+        }}
+        onBlur={() => {
+          props.saveSet(true);
+        }}
+      />
+      <div className="col-span-1 flex gap-5">
+        <input
+          className=" h-10 grow appearance-none rounded-md border border-gray-300 bg-gray-100 px-4 font-normal leading-normal outline-none hover:border-gray-400 focus:border-blue-500 focus:bg-gray-200"
+          type="text"
+          placeholder="Enter a definition..."
+          onChange={(e) => {
+            card = props.editCard(e, card, "definition");
+          }}
+          onBlur={() => {
+            card.isEdited = true;
+            props.saveSet(true);
+          }}
+        />
+        <button
+          className="h-10 w-10 rounded-md bg-red-500 text-white"
+          onClick={() => props.deleteCard(card)}
+        >
+          <h1 className="text-md">X</h1>
+        </button>
+      </div>
     </div>
   );
 }
