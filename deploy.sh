465dcfe4e7f7598086cc7ac9bd0f7647a6550130
Paridax <tgavdaly@gmail.com>
2023-02-24 17:10:28 -0500

    fix issue with deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
-"/usr/bin/pm2" stop "quizzable"
-cd "/server/quizzable" && "/usr/bin/pm2" start npm --name "quizzable" -- run start -- -p 8089
-"/server/quizzable/pocketbase/quizzable" serve
+NEXTPORT=8089
+POCKETPORT=8090
+
+"/usr/bin/pm2" delete "quizzable" || echo "No process found, continuing..."
+cd "/server/quizzable" && "/usr/bin/pm2" start npm --name "quizzable" -- run start -- -p $NEXTPORT
+"/server/quizzable/pocketbase/quizzable" serve || fuser -n tcp -k $POCKETPORT && "/server/quizzable/pocketbase/quizzable" serve
