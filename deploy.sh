da43441c906df60fdc728a1056f467529a32227d
Paridax <tgavdaly@gmail.com>
2023-02-25 23:05:00 -0500

    Edit deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 NEXTPORT=8089
 POCKETPORT=8090
 
-"/usr/bin/pm2" delete "quizzable" || echo "No process found, continuing..."
-cd "/server/quizzable" && "/usr/bin/pm2" start npm --name "quizzable" -- run start -- -p $NEXTPORT
+"/usr/bin/pm2" restart "quizzable" && echo "Restarting Service" || cd "/server/quizzable" && "/usr/bin/pm2" start npm --name "quizzable" -- run start -- -p $NEXTPORT && echo "Starting Service"
 "/server/quizzable/pocketbase/quizzable" serve || fuser -n tcp -k $POCKETPORT && "/server/quizzable/pocketbase/quizzable" serve
